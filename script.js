b6bdd7f07744e3d7a859935aac5d9197b73387cc
hayathdt <hayat.haddouti2@gmail.com>
2024-11-26 14:32:53 +0100

    add JavaScript and JSON files for API integration/ Delete SVG arrows, switched to icon set

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.getElementById("container");
+const imgElement = document.getElementById("pokemon-image");
+const buttonUp = document.getElementById("pad-up");
+const buttonDown = document.getElementById("pad-down");
+const buttonLeft = document.getElementById("pad-left");
+const buttonRight = document.getElementById("pad-right");
+
+
+
+
+
