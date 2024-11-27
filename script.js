8fcd0a91d2d0394743b2ee09640971c97ca1e14d
hayathdt <hayat.haddouti2@gmail.com>
2024-11-27 20:11:30 +0100

    fix: debug API, add new image type, and update CSS styling

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const pokedexleft = document.getElementById("pokedex-left");
 const nameElement = document.getElementById("pokemon-name");
 const typeElement = document.getElementById("pokemon-type");
 const imgElement = document.getElementById("pokemon-image");
+const imgTypeElement = document.getElementById("image-type");
+const imgTypeElement2 = document.getElementById("image-type2");
 const container = document.getElementById("container");
 
 const buttonUp = document.getElementById("pad-up");
@@ -9,32 +11,41 @@ const buttonDown = document.getElementById("pad-down");
 const buttonLeft = document.getElementById("pad-left");
 const buttonRight = document.getElementById("pad-right");
 
-let currentPokemonIndex = 0;
+let currentPokemonIndex = 1;
+let pokemons = [];
 
 fetch("apipokedex.json")
   .then(response => response.json())
-  .then( pokemons => {
-    console.log(pokemons);
+  .then(data => {
+    pokemons = data;
+  });
 
   function displayPokemon(index) {
-     const pokemon = apipokedex[index];
+     const pokemon = pokemons[index];
      if (pokemon) {
         imgElement.src = pokemon.image;
-        nameElement.textContent = `Nom : ${pokemon.name}`
-        typeElement.textContent = `Type : ${pokemon.type.join(", ")}`; 
+        nameElement.innerText = `${pokemon.name}`
+        typeElement.innerText = `${pokemon.apiTypes[0].name}`;
+        imgTypeElement.src = pokemon.apiTypes[0].image;
+        if (pokemon.apiTypes[1]) {
+          imgTypeElement2.src = pokemon.apiTypes[1].image;
+          typeElement.innerText += "/" + `${pokemon.apiTypes[1].name}`;
+        }
+
+        }
      }
-  }
+
+  
 displayPokemon(currentPokemonIndex);
 
-buttonLeft.addEventListerner("click", () =>{
-     currentPokemonIndex = (currentPokemonIndex - 1 + apipokedex.length) % apipokedex.length;
+buttonLeft.addEventListener("click", () =>{
+     currentPokemonIndex = (currentPokemonIndex - 1 + pokemons.length) % pokemons.length;
+     console.log(currentPokemonIndex);
      displayPokemon(currentPokemonIndex);
 });
-buttonRight.addEventListerner("click", () =>{
-     currentPokemonIndex = (currentPokemonIndex + 1) % apipokedex.length;
+
+buttonRight.addEventListener("click", () =>{
+     currentPokemonIndex = (currentPokemonIndex + 1) % pokemons.length;
+     console.log(currentPokemonIndex);
      displayPokemon(currentPokemonIndex);
-})
-.catch(error => {
-  console.error("Erreur dans le chargement :", error);
-}
-); 
+});
