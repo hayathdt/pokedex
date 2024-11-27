1dff9925ace301c5e15fb0d4ec814c1f00c54210
hayathdt <hayat.haddouti2@gmail.com>
2024-11-27 21:53:23 +0100

    Improve UI with centered types, better background scaling, and API display for Pokémon details

--- script.js
+++ script.js
@@ -11,6 +11,14 @@ const buttonDown = document.getElementById("pad-down");
 const buttonLeft = document.getElementById("pad-left");
 const buttonRight = document.getElementById("pad-right");
 
+const pokemonName2 = document.getElementById("pokemon-name2");
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defense = document.getElementById("defense");
+const speed = document.getElementById("speed");
+const specialAttack = document.getElementById("special-attack");
+const specialDefense = document.getElementById("special-defense");
+
 let currentPokemonIndex = 1;
 let pokemons = [];
 
@@ -27,11 +35,20 @@ fetch("apipokedex.json")
         nameElement.innerText = `${pokemon.name}`
         typeElement.innerText = `${pokemon.apiTypes[0].name}`;
         imgTypeElement.src = pokemon.apiTypes[0].image;
+        pokemonName2.innerHTML = `<strong>Nom: </strong>${pokemon.name}`;
+        hp.innerHTML = `<strong>HP: </strong>${pokemon.stats.HP}`;
+        attack.innerHTML = `<strong>Attaque: </strong>${pokemon.stats.attack}`;
+        defense.innerHTML = `<strong>Défense: </strong>${pokemon.stats.defense}`;
+        speed.innerHTML = `<strong>Vitesse: </strong>${pokemon.stats.speed}`;
+        specialAttack.innerHTML = `<strong>Attaque spéciale: </strong>${pokemon.stats.special_attack}`;
+        specialDefense.innerHTML = `<strong>Défense spéciale: </strong>${pokemon.stats.special_defense}`;
         if (pokemon.apiTypes[1]) {
           imgTypeElement2.src = pokemon.apiTypes[1].image;
           typeElement.innerText += "/" + `${pokemon.apiTypes[1].name}`;
         }
-
+        else{
+          imgTypeElement2.src = "";
+        }
         }
      }
 
